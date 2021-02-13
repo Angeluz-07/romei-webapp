70ec8bb360d588bb1723c6610ce81517114306ab
roberto <alexandro.ramr777@gmail.com>
2021-02-13 17:06:36 -0500

    feat: add hot update of payment-registers

--- vue_ui/src/components/PaymentsSheetRow.vue
+++ vue_ui/src/components/PaymentsSheetRow.vue
@@ -1,9 +1,10 @@
 <template>
     <tr>
-        <td><input v-model="value"  type="text" class="form-control"></td>
-        <td><input v-model="name" type="text" class="form-control"></td>
-        <td><input v-model="description"  type="text" class="form-control"></td>
+        <td><input v-model="value" v-on:input="updatePaymentsRegister" type="text" class="form-control"></td>
+        <td><input v-model="name" v-on:input="updatePaymentsRegister" type="text" class="form-control"></td>
+        <td><input v-model="description"  v-on:input="updatePaymentsRegister" type="text" class="form-control"></td>
         <td>
+        <div v-if="loading" class="spinner-border text-secondary" role="status"></div>
         <button type="button" class="btn btn-danger" @click="$emit('removePaymentsRegister', paymentsRegister.id)">x</button>
         </td>
     </tr>
@@ -11,13 +12,14 @@
 
 <script>
 
-//const BASE_URL = 'http://localhost:8000/dailyControl/api';
+const BASE_URL = 'http://localhost:8000/dailyControl/api';
 
 export default {
     name: 'PaymentsSheetRow',
     props: ['paymentsRegister'],
     data() {
         return {
+            loading : false,
             value : "",
             name: "",
             description: ""
@@ -33,6 +35,23 @@ export default {
             this.value = this.paymentsRegister.value;
             this.name = this.paymentsRegister.name;
             this.description = this.paymentsRegister.description;
+        },
+        updatePaymentsRegister() {
+            let _data = {
+                value : this.value,
+                name : this.name,
+                description : this.description
+            }
+            this.loading = true;
+            fetch(`${BASE_URL}/payments-registers/${this.paymentsRegister.id}`, {
+                method: "PATCH",
+                body: JSON.stringify(_data),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+            })
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(err => console.log(err))
+            .finally(() => this.loading = false);
         },
     }
 }
