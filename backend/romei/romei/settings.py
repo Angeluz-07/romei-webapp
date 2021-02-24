70ab4358e19076d6449fcdd2152fbbfa7d6c606b
roberto <alexandro.ramr777@gmail.com>
2021-02-23 23:43:01 -0500

    fix: remove UI_SERVICE env var

--- backend/romei/romei/settings.py
+++ backend/romei/romei/settings.py
@@ -165,11 +165,8 @@ APPEND_SLASH = False
 ALLOWED_HOSTS=['*']
 
 
-check_env_variables(['UI_SERVICE'])
-UI_SERVICE = os.environ.get('UI_SERVICE')
-
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
-    UI_SERVICE # Here was the problem indeed and it has to be http://localhost:3000, not http://localhost:3000/
+    'http://localhost:8081' # Here was the problem indeed and it has to be http://localhost:3000, not http://localhost:3000/
 ]
